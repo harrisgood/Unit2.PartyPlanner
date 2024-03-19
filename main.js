ddaae75cf6d519941f5897a021f9e0265f0f1d6f
Harris <harrisgoodhand@DESKTOP-51ACIT7>
2024-03-19 17:16:50 -0400

    split up event information so it's a little easier to read

--- main.js
+++ main.js
@@ -6,23 +6,67 @@ const state = {
 events: []
 }
 
-async function getEvents () {
-try{
-// if theres no errors, run this code block
+// set constants for all of our HTML lists so we can easily add to them
+const namesListUL = document.querySelector(`#partyNames`)
+const descriptionsListUL = document.querySelector(`#partyDescriptions`)
+const datesListUL = document.querySelector(`#partyDates`)
+const timesListUL = document.querySelector(`#partyTimes`)
+const locationsListUL = document.querySelector(`#partyLocations`)
 
-// fetch data from api (check it with a console log)
-const apiParties = await fetch(API_URL) 
-console.log(apiParties)
+// special function to ensure that our page runs in the right order without getting ahead of itself
+async function renderPage() {
+  await getEvents() // dont do anything until this line finishes
+  renderState()
+}
+renderPage()
+async function getEvents() {
+  try{
+  // if theres no errors, run this code block
 
-// parse data into useable format
-const jsonParties = await apiParties.json()
-console.log(jsonParties.data)
+  // fetch data from api (check it with a console log)
+  const apiParties = await fetch(API_URL) 
 
-} catch (error){
-  // if we had an error, log it
-  console.log(error)
+  // parse data into useable format
+  const jsonParties = await apiParties.json()
+
+  // update state with parsed data
+  state.events = jsonParties.data
+
+  } catch (error){
+    // if we had an error, log it
+    console.log(error)
+  }
 }
 
-}
+function renderState() {
+  
+  // loop through events
+  state.events.forEach( newParty => {
+      // create blank new li 
+    const newName = document.createElement(`li`)
+    const newDescription = document.createElement(`li`)
+    const newDate = document.createElement(`li`)
+    const newTime = document.createElement(`li`)
+    const newLocation = document.createElement(`li`)
 
-getEvents()
+    // handle date and time split
+    const dateAndTime = newParty.date
+    const indexOfT = dateAndTime.indexOf(`T`)
+    const newPartyDate = dateAndTime.slice(0, indexOfT)
+    const newPartyTime = dateAndTime.slice(indexOfT+1, indexOfT+6)
+
+    // change innerHTML of new 'li's
+    newName.innerHTML = `${newParty.name}`
+    newDescription.innerHTML = `${newParty.description}`
+    newDate.innerHTML = `${newPartyDate}` // TODODOODODO
+    newTime.innerHTML = `${newPartyTime}`
+    newLocation.innerHTML = `${newParty.location}`
+    
+    // add to ul `partyList`
+    namesListUL.appendChild(newName)
+    descriptionsListUL.appendChild(newDescription)
+    datesListUL.appendChild(newDate)
+    timesListUL.appendChild(newTime)
+    locationsListUL.appendChild(newLocation)
+  })  
+}
