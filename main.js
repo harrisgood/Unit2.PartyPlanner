5cf68a146556f7c6f363e1bfde4dd70478fa0073
Harris <harrisgoodhand@DESKTOP-51ACIT7>
2024-03-19 21:47:53 -0400

    changed unorderd lists to divs and paragraphs, much better looking git push

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 // store api url in an easily accessible constant
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/$2402-FTB-ET-WEB-FT/events`
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2402-FTB-ET-WEB-FT/events`
 
 // store all of our site data in state, in this case just any events going on
 const state = {
 events: []
 }
 
-// set constants for all of our HTML lists so we can easily add to them
-const namesListUL = document.querySelector(`#partyNames`)
-const descriptionsListUL = document.querySelector(`#partyDescriptions`)
-const datesListUL = document.querySelector(`#partyDates`)
-const timesListUL = document.querySelector(`#partyTimes`)
-const locationsListUL = document.querySelector(`#partyLocations`)
+// set constant for all of our HTML list section so we can easily add events to it
+const partyListSection = document.querySelector(`#partyList`)
 
 // special function to ensure that our page runs in the right order without getting ahead of itself
 async function renderPage() {
@@ -20,15 +16,15 @@ async function renderPage() {
 }
 renderPage()
 async function getEvents() {
-  try{
   // if theres no errors, run this code block
+  try{  
 
-  // fetch data from api (check it with a console log)
+  // fetch data from api
   const apiParties = await fetch(API_URL) 
 
-  // parse data into useable format
+  // parse data into more useable format
   const jsonParties = await apiParties.json()
-    console.log(jsonParties)
+    
   // update state with parsed data
   state.events = jsonParties.data
 
@@ -38,35 +34,47 @@ async function getEvents() {
   }
 }
 
+// updates the viewer's page to display data from our site's state
 function renderState() {
-  
-  // loop through events
+  // loop through all events in state
   state.events.forEach( newParty => {
-      // create blank new li 
-    const newName = document.createElement(`li`)
-    const newDescription = document.createElement(`li`)
-    const newDate = document.createElement(`li`)
-    const newTime = document.createElement(`li`)
-    const newLocation = document.createElement(`li`)
+    // create blank new paragraphs for each `property` of an event we want to display
+    const newName = document.createElement(`p`)
+    const newDescription = document.createElement(`p`)
+    const newDate = document.createElement(`p`)
+    const newTime = document.createElement(`p`)
+    const newLocation = document.createElement(`p`)
 
-    // handle date and time split
+    // split the date and time into a more easily displayable format
+    // get date from api (example return: "2023-07-23T20:18:02.000Z")
     const dateAndTime = newParty.date
+    
+    // find index of convienient T between date and time in current data
     const indexOfT = dateAndTime.indexOf(`T`)
+
+    // grab everything before the T
     const newPartyDate = dateAndTime.slice(0, indexOfT)
+    // grab the 5 characters after the T
     const newPartyTime = dateAndTime.slice(indexOfT+1, indexOfT+6)
 
-    // change innerHTML of new 'li's
+    // change innerHTML of new paragraphs to respective property
     newName.innerHTML = `${newParty.name}`
     newDescription.innerHTML = `${newParty.description}`
-    newDate.innerHTML = `${newPartyDate}` // TODODOODODO
+    newDate.innerHTML = `${newPartyDate}`
     newTime.innerHTML = `${newPartyTime}`
     newLocation.innerHTML = `${newParty.location}`
     
-    // add to ul `partyList`
-    namesListUL.appendChild(newName)
-    descriptionsListUL.appendChild(newDescription)
-    datesListUL.appendChild(newDate)
-    timesListUL.appendChild(newTime)
-    locationsListUL.appendChild(newLocation)
+    // create new blank div to construct an 'event div' holding all necessary data
+    const newPartyDiv = document.createElement(`div`)
+
+    // add paragrphs with data to our empty event div
+    newPartyDiv.appendChild(newName)
+    newPartyDiv.appendChild(newDescription)
+    newPartyDiv.appendChild(newDate)
+    newPartyDiv.appendChild(newTime)
+    newPartyDiv.appendChild(newLocation)
+
+    // add our loaded up newParty div to partyList section on index.html
+    partyListSection.appendChild(newPartyDiv)
   })  
 }
